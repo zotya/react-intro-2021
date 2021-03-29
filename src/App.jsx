764837a1d29148a1cbcaaf2c0103503dadfa40be
Sabin Marcu <sabinmarcu@gmail.com>
2021-03-29 15:07:35 +0300

    class counter implemented

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
-import { Button } from './components/Button';
+import { Counter } from './components/CounterClass';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React Old School
-        </a>
-        <Button color="primary">Primary</Button>
-        <Button color="secondary">Secondary</Button>
-        <Button color="primary" fontSize={30} raised>Primary</Button>
-        <Button
-          color="secondary"
-          raised
-          fontSize={30}
-          style={{ color: 'teal' }}
-        >
-          Secondary
-        </Button>
-      </header>
+      <Counter />
     </div>
   );
 }
