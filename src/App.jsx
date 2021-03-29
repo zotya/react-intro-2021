4b71343cbb238c03e5ecb87c445efc0e93d45aa6
Sabin Marcu <sabinmarcu@gmail.com>
2021-03-29 14:17:45 +0300

    basic button

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import { Button } from './components/Button';
 
 function App() {
   return (
@@ -14,6 +15,17 @@ function App() {
         >
           Learn React Old School
         </a>
+        <Button color="primary">Primary</Button>
+        <Button color="secondary">Secondary</Button>
+        <Button color="primary" fontSize={30} raised>Primary</Button>
+        <Button
+          color="secondary"
+          raised
+          fontSize={30}
+          style={{ color: 'teal' }}
+        >
+          Secondary
+        </Button>
       </header>
     </div>
   );
