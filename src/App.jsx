d58ee80100e29a8c9dbdd5dfabc670da7d15ad81
Zoltan Szabo <zoltan.szabo@eaudeweb.ro>
2021-04-05 03:05:25 +0300

    Playing with react-measure

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,9 @@ import {
   Switch,
   Route,
 } from 'react-router-dom';
+import { useDebugValue } from 'react';
+import { withContentRect } from 'react-measure';
+
 import { Container } from './components/Layout';
 import { ThemeProvider } from './contexts/theme';
 import { Navbar } from './components/Navbar';
@@ -12,31 +15,37 @@ import { MoviePage } from './pages/Movie';
 
 import styles from './App.module.css';
 
-function App() {
+function App({ measureRef, measure, contentRect }) {
+  useDebugValue(measureRef);
+  useDebugValue(measure);
+  useDebugValue(contentRect);
   return (
     <ThemeProvider>
 
       <Router>
-        <div className="App">
+        <div className={styles.App}>
           <Navbar>
             Movie DB
           </Navbar>
-          <Container
-            direction="vertical"
-            className={styles.Container}
-          >
-            <Switch>
-              <Route exact path="/movie/:id" component={MoviePage} />
-              <Route exact path="/" component={HomePage} />
-              <Route path="/">
-                <h1>404</h1>
-              </Route>
-            </Switch>
-          </Container>
+          <div ref={measureRef}>
+            <Container
+              direction="vertical"
+              className={styles.Container}
+            >
+              <Switch>
+                <Route exact path="/movie/:id" component={MoviePage} />
+                <Route exact path="/" component={HomePage} />
+                <Route path="/">
+                  <h1>404</h1>
+                </Route>
+              </Switch>
+            </Container>
+          </div>
+          <pre>{JSON.stringify(contentRect, null, 2)}</pre>
         </div>
       </Router>
     </ThemeProvider>
   );
 }
 
-export default App;
+export default withContentRect(['client', 'offset', 'scroll', 'bounds', 'margin'])(App);
